72fea4c67261850cd24f2b60d28b49ddacff555b
grolent <r.deprezz@laposte.net>
2016-06-05 15:55:44 +0200

    Logique ok ajuster graphisme

--- tempProject/app/pages/equation-degre-2/equation-degre-2.js
+++ tempProject/app/pages/equation-degre-2/equation-degre-2.js
@@ -20,17 +20,44 @@ export class EquationDegre2 {
     this.a = this.equationDegre2Form.controls['a'];
     this.b = this.equationDegre2Form.controls['b'];
     this.c = this.equationDegre2Form.controls['c'];
+    this.result = "";
   }
   calculSecondDegre(event) {
     var facA = parseFloat(this.a._value);
     var facB = parseFloat(this.b._value);
     var facC = parseFloat(this.c._value);
+    var racine1;
+    var racine2;
+    var strResult = "";
+
+    if(facA === 0){
+      strResult = "a doit être différent de 0";
+    }
+    else{
+      var p = Math.pow(facB, 2) - (4*facA*facC);
+      if(p === 0){
+        racine1 = ((-facB)/(2*facA)).toFixed(4);
+        strResult = "Le polynôme a 1 racine : R1="+racine1;
+      }
+      else if(p > 0){
+        racine1 = ((-facB + (Math.sqrt(p)))/(2*facA)).toFixed(4);
+        racine2 = ((-facB - (Math.sqrt(p)))/(2*facA)).toFixed(4);
+        strResult = "Le polynôme a 2 racines : R1="+racine1+" et R2="+racine2;
+      }
+      else if(p < 0){
+        var racinePartieReelle;
+        var racinePartieImaginaire;
+        racinePartieReelle = (-facB/(2*facA)).toFixed(4);
+        racinePartieImaginaire = ((Math.sqrt(-p))/(2*facA)).toFixed(4);
+        racine1 = "("+racinePartieReelle+") +i ("+racinePartieImaginaire+")";
+        racine2 = "("+racinePartieReelle+") -i ("+racinePartieImaginaire+")";
+        strResult = "Le polynôme a 2 racines : R1="+racine1+" et R2="+racine2;
+      }
+    }
+    this.result = strResult;
 
 
     event.preventDefault();
-  }
-  testController(event) {
-    console.log('test controller: ', this);
   }
 
 }
