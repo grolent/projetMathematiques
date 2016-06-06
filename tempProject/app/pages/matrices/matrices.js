4e684cd146b14926a23812b8266b2185ac0cbda2
grolent <r.deprezz@laposte.net>
2016-06-06 10:47:43 +0200

    functions to add and soustract matrices

--- tempProject/app/pages/matrices/matrices.js
+++ tempProject/app/pages/matrices/matrices.js
@@ -13,10 +13,52 @@ export class Matrices {
   }
   constructor(form) {
     this.matrices = [];
+
+    this.matriceTestA = [
+      [1,2,3,4],
+      [4,3,2,1],
+      [5,6,7,8]
+    ];
+
+    this.matriceTestB = [
+      [4,3,2,1],
+      [1,2,3,4],
+      [8,7,6,5]
+    ];
+    this.matriceTestResult = [];
   }
   addMatrice(matrice){
     this.matrices.push(matrice);
   }
-
+  additionMatrices(matriceA, matriceB){
+    if( (matriceA.length !== matriceB.length) || (matriceA[0].length !== matriceB[0].length) ){
+      return null;
+    }
+    else{
+      var matriceResult = [];
+      for(var i = 0; i<matriceA.length; i++){
+        matriceResult[i] = [];
+        for(var j = 0; j<matriceA[i].length; j++){
+          matriceResult[i][j] = matriceA[i][j] + matriceB[i][j];
+        }
+      }
+      return matriceResult;
+    }
+  }
+  soustractionMatrices(matriceA, matriceB){
+    if( (matriceA.length !== matriceB.length) || (matriceA[0].length !== matriceB[0].length) ){
+      return null;
+    }
+    else{
+      var matriceResult = [];
+      for(var i = 0; i<matriceA.length; i++){
+        matriceResult[i] = [];
+        for(var j = 0; j<matriceA[i].length; j++){
+          matriceResult[i][j] = matriceA[i][j] - matriceB[i][j];
+        }
+      }
+      return matriceResult;
+    }
+  }
 
 }
